9eceeca494466d4eca9207315a02bd5fe0f97e36
Sultanbai Almaz <aldeco312@gmail.com>
2023-07-08 18:09:24 +0600

    commit1

--- month3_collectionView/MovieModel.swift
+++ month3_collectionView/MovieModel.swift
@@ -22,18 +22,16 @@ struct MovieModel {
 struct MovieData {
     let movies: [MovieModel] = [
         
-        MovieModel(movieImage: UIImage(named: "wrangler")!, movieName: "Car Movie", moviePrice:  "5$"),
-        MovieModel(movieImage: UIImage(named: "wrangler")!, movieName: "Car Movie", moviePrice:  "5$"),
-        MovieModel(movieImage: UIImage(named: "wrangler")!, movieName: "Car Movie", moviePrice:  "5$"),
-        MovieModel(movieImage: UIImage(named: "wrangler")!, movieName: "Car Movie", moviePrice:  "5$"),
-        MovieModel(movieImage: UIImage(named: "wrangler")!, movieName: "Car Movie", moviePrice:  "5$"),
-        MovieModel(movieImage: UIImage(named: "wrangler")!, movieName: "Car Movie", moviePrice:  "5$"),
-        MovieModel(movieImage: UIImage(named: "wrangler")!, movieName: "Car Movie", moviePrice:  "5$"),
-        MovieModel(movieImage: UIImage(named: "wrangler")!, movieName: "Car Movie", moviePrice:  "5$"),
-        MovieModel(movieImage: UIImage(named: "wrangler")!, movieName: "Car Movie", moviePrice:  "5$"),
-        MovieModel(movieImage: UIImage(named: "wrangler")!, movieName: "Car Movie", moviePrice:  "5$"),
-        MovieModel(movieImage: UIImage(named: "wrangler")!, movieName: "Car Movie", moviePrice:  "5$"),
-        MovieModel(movieImage: UIImage(named: "wrangler")!, movieName: "Car Movie", moviePrice:  "5$")
+        MovieModel(movieImage: UIImage(named: "movie1")!, movieName: "Guardians", moviePrice:  "5$"),
+        MovieModel(movieImage: UIImage(named: "movie2")!, movieName: "Spirited", moviePrice:  "5$"),
+        MovieModel(movieImage: UIImage(named: "movie3")!, movieName: "Spider-Man", moviePrice:  "5$"),
+        MovieModel(movieImage: UIImage(named: "movie4")!, movieName: "Hokus-Pokus", moviePrice:  "5$"),
+        MovieModel(movieImage: UIImage(named: "movie5")!, movieName: "Capitan America", moviePrice:  "5$"),
+        MovieModel(movieImage: UIImage(named: "movie6")!, movieName: "Paw Patrol", moviePrice:  "5$"),
+        MovieModel(movieImage: UIImage(named: "movie7")!, movieName: "Killer Shark", moviePrice:  "5$"),
+        MovieModel(movieImage: UIImage(named: "movie8")!, movieName: "Flash", moviePrice:  "5$"),
+        MovieModel(movieImage: UIImage(named: "movie9")!, movieName: "Black Widow", moviePrice:  "5$"),
+        MovieModel(movieImage: UIImage(named: "movie10")!, movieName: "Avatar 2", moviePrice:  "5$"),
         
         ]
 }
